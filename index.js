ab7eab12d7603f1d4f669ec10d80b5d77f0fa674
Mustafizur <sayhemrahman@gmail.com>
2021-10-31 19:17:09 +0600

    all corrected

--- index.js
+++ index.js
@@ -14,9 +14,6 @@ app.use(express.json());
 
 
 
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.swu9d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
-
 // Without Environement variables
 // const uri = 'mongodb+srv://mustafizPractice:qH9d5y2tAs90BDGI@cluster0.gqmhk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
@@ -42,6 +39,11 @@ async function run() {
         const bookingCollection = client.db("MustafizToursimSite").collection("booking");
 
         const blogCollection = client.db("MustafizToursimSite").collection("blog");
+
+        /*----------------------------------
+         Travel API
+        --------------------------------------------*/
+
 
         // GET API
 
@@ -73,7 +75,9 @@ async function run() {
         });
 
 
-
+        /*----------------------------------
+                 Booking API
+     --------------------------------------------*/
 
         // GET API FOR  COnfirm Booking
 
@@ -130,9 +134,9 @@ async function run() {
 
 
 
-        // POST API FOR ADD Travel BLOG
-
-        // GET API FOR Travel BloG
+        /*----------------------------------
+         Blog API
+        --------------------------------------------*/
 
         // GET API
 
@@ -166,8 +170,6 @@ async function run() {
 
 
 
-
-
     }
     finally {
         // await client.close()
@@ -178,7 +180,7 @@ run().catch(console.dir);
 
 
 app.get('/', (req, res) => {
-    res.send('Mustafiz Assignment elevent Toursim delivery site!')
+    res.send('Mustafiz Assignment eleven Toursim delivery site!')
 })
 
 
@@ -188,7 +190,7 @@ app.listen(port, () => {
 })
 
 // Deploument Server link in Heroku
-// https://shrouded-eyrie-89480.herokuapp.com/mustafizeducation
+// https://frightening-spell-88460.herokuapp.com/
 //  1. Steps install Heroku CLI 
 //  2. heroku login
 //  3. heroku create
